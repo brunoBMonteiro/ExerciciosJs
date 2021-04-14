7361f7f01a3720c3a34e9255c3137340ab4b2d8d
brunoBMonteiro <brunobilheri@hotmail.com>
2021-04-14 16:30:07 -0300

    manipulando arrays

---
+++ jsExercicios/manipulandoArrays.js
@@ -0,0 +1,13 @@
+let techs = ["html", "css", "js"]
+
+// adiciona item no fim
+techs.push("nodejs")
+// adiciona no começo
+techs.unshift("sql")
+
+// remove do começo
+techs.shift()
+// remove do fim
+techs.pop()
+
+console.log(techs)
