4b3d2c826450e0b0f3d2ce8f74f3711cd2744989
brunoBMonteiro <brunobilheri@hotmail.com>
2021-04-14 10:02:38 -0300

    Maiusculas em minusculas com toUpperCase e toLowerCase

---
+++ jsExercicios/casasDecimais.js
@@ -0,0 +1,2 @@
+let number = 645648548.54545
+console.log(number.toFixed(2).replace(".", ","))
---
+++ jsExercicios/maiusculasEmMinusculas.js
@@ -0,0 +1,6 @@
+let word = "Programar é muito bacana!"
+
+console.log(word.toUpperCase())
+
+let world = "Programar é muito bacana!"
+console.log(word.toLowerCase())
