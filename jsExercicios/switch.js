ae60f6db1b392591fbe6917dd6040f6895444ab0
brunoBMonteiro <brunobilheri@hotmail.com>
2021-04-15 21:35:04 -0300

    switch

---
+++ jsExercicios/switch.js
@@ -0,0 +1,14 @@
+let expression = 'c'
+
+switch (expression) {
+    case 'a':
+        // codigo
+        console.log('a')
+        break
+    case 'b':
+        console.log('b')
+        break
+    default:
+        console.log('default')
+        break
+}
