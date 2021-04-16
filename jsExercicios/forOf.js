e122a03d9545fe0dcad0351c1aeb6c218423634c
brunoBMonteiro <brunobilheri@hotmail.com>
2021-04-16 14:59:53 -0300

    for of

---
+++ jsExercicios/forOf.js
@@ -0,0 +1,13 @@
+// let name = 'Bruno'
+// let names = ['João', 'Paulo', 'Mayk']
+
+// for(let char of name) {
+//     console.log(char)
+// }
+
+let name = 'Bruno'
+let names = ['João', 'Paulo', 'Mayk']
+
+for(let name of names) {
+    console.log(name)
+}
